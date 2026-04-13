967f44d39abe182dd6d36ae1e7965b5a0eb65353
Tanmay Kalra <151675979+TanmayKalra09@users.noreply.github.com>
2026-04-13 10:28:03 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int c=0;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,1,-1};
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]=='0'){
+                    continue;
+                }
+                c++;
+                grid[i][j]='0';
+                Queue<int[]> queue = new LinkedList<>();
+                queue.offer(new int[]{i,j});
+                while(!queue.isEmpty()){
+                    int[] curr = queue.poll();
+                    for(int k=0;k<4;k++){
+                        int nr = curr[0]+dx[k];
+                        int nc = curr[1]+dy[k];
+                        if(nr<0 || nc<0 || nr>=grid.length || nc>=grid[0].length){
+                            continue;
+                        }
+                        if(grid[nr][nc]!='0'){
+                            queue.offer(new int[]{nr,nc});
+                            grid[nr][nc]='0';
+                        }
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
